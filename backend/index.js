5e770c6580b4b49b2819ca2e65bf55e7e2d15bff
ShafayetAhmad <shafayet.ahmad1@gmail.com>
2023-12-31 00:49:04 +0600

    db credentials modified

--- backend/index.js
+++ backend/index.js
@@ -3,22 +3,29 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+app.use(express.json());
 
 const { Client } = require("pg");
 const client = new Client({
-  host: "localhost",
-  user: "postgres",
-  port: 5432,
-  password: "kanon890@",
-  database: "postgres",
+  host: process.env.DB_host,
+  user: process.env.DB_user,
+  port: process.env.DB_port,
+  password: process.env.DB_password,
+  database: process.env.DB_database,
 });
 
 client.connect();
 
-client.query("SELECT * FROM users", (err, res) => {
-  if (!err) {
-    console.log(res.rows);
-  } else {
-    console.log(err);
-  }
+app.get("/getUsers", (req, res) => {
+  client.query("SELECT * FROM users", (err, result) => {
+    if (!err) {
+      res.json(result.rows);
+    } else {
+      res.status(500).json({ error: err.message });
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Listening at port ${port}`);
 });
