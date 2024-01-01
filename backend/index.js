5c2efda34db55b55975dc209b991947464307bf6
ShafayetAhmad <shafayet.ahmad1@gmail.com>
2024-01-01 14:32:23 +0600

    response on question implemented

--- backend/index.js
+++ backend/index.js
@@ -64,9 +64,9 @@ app.post("/addUser", (req, res) => {
 });
 
 app.post("/createConversation", (req, res) => {
-  const { title, user, question } = req.body;
+  const { title, user } = req.body;
   const is_shared = false;
-  console.log(title, user, question);
+  console.log(title, user);
   const userEmail = user.toLowerCase().replace(/\./g, "");
 
   client.query(
@@ -76,27 +76,15 @@ app.post("/createConversation", (req, res) => {
       if (!err) {
         if (userResult.rows.length > 0) {
           const userId = userResult.rows[0].user_id;
-          console.log(userId);
+
           client.query(
             `INSERT INTO conversations (title, user_id, is_shared) VALUES ($1, $2, $3) RETURNING conversation_id`,
             [title, userId, is_shared],
             (err, conversationResult) => {
               if (!err) {
-                client.query(
-                  `INSERT INTO messages ( conversation_id, user_id, message_text) VALUES ($1, $2, $3)`,
-                  [
-                    conversationResult.rows[0].conversation_id,
-                    userId,
-                    question,
-                  ],
-                  (err, messageResult) => {
-                    if (!err) {
-                      res.json(messageResult.rows);
-                    } else {
-                      res.status(500).json({ error: err.message });
-                    }
-                  }
-                );
+                var convId = conversationResult.rows[0].conversation_id;
+                console.log(convId);
+                res.json(convId);
               } else {
                 res.status(500).json({ error: err.message });
               }
@@ -107,6 +95,142 @@ app.post("/createConversation", (req, res) => {
         }
       } else {
         res.status(500).json({ error: err.message });
+      }
+    }
+  );
+});
+
+app.post("/askFirstMessage", (req, res) => {
+  const { conversationId, title, email } = req.body;
+
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let answerMessage = [];
+  let answerText = "";
+
+  for (let i = 0; i < 200; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    answerText += characters.charAt(randomIndex);
+  }
+
+  client.query(
+    `UPDATE conversations SET title = $1 WHERE conversation_id = $2`,
+    [title, conversationId],
+    (err, result) => {
+      if (!err) {
+        const userEmail = email.toLowerCase().replace(/\./g, "");
+        client.query(
+          `SELECT user_id FROM users WHERE email = $1`,
+          [userEmail],
+          (err, userResult) => {
+            if (!err) {
+              const userId = userResult.rows[0].user_id;
+              client.query(
+                `INSERT INTO messages (conversation_id, message_text, user_id) VALUES ($1, $2, $3) RETURNING *`,
+                [conversationId, title, userId],
+                (err, questionResult) => {
+                  answerMessage.push(questionResult.rows[0]);
+                  if (!err) {
+                    client.query(
+                      `INSERT INTO messages (conversation_id, message_text, user_id) VALUES ($1, $2, $3) RETURNING *`,
+                      [conversationId, answerText, 3],
+                      (err, answerResult) => {
+                        answerMessage.push(answerResult.rows[0]);
+                        if (!err) {
+                          console.log(answerMessage);
+                          res.json(answerMessage);
+                        } else {
+                          res.status(500).json({ error: err.message });
+                        }
+                      }
+                    );
+                  } else {
+                    res.status(500).json({ error: err.message });
+                  }
+                }
+              );
+            } else {
+              res.status(500).json({ error: err.message });
+            }
+          }
+        );
+      } else {
+        res.status(500).json({ error: err.message });
+      }
+    }
+  );
+});
+
+app.get("/getConversations", (req, res) => {
+  client.query(`SELECT * FROM conversations`, (err, result) => {
+    if (!err) {
+      console.log(result.rows);
+      res.json(result.rows);
+    } else {
+      res.status(500).json({ error: err.message });
+    }
+  });
+});
+
+app.get("/getMessages", (req, res) => {
+  const conversationId = req.query.conversationId;
+  client.query(
+    `SELECT * FROM messages WHERE conversation_id = $1 ORDER BY message_id ASC`,
+    [conversationId],
+    (err, result) => {
+      if (!err) {
+        console.log(result.rows);
+        res.json(result.rows);
+      } else {
+        res.status(500).json({ error: err.message });
+      }
+    }
+  );
+});
+
+app.post("/addMessage", async (req, res) => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let answerMessage = [];
+  let answerText = "";
+
+  for (let i = 0; i < 200; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    answerText += characters.charAt(randomIndex);
+  }
+
+  const { conversationId, question } = req.body;
+
+  client.query(
+    `SELECT user_id FROM conversations WHERE conversation_id = $1`,
+    [conversationId],
+    (err, userResult) => {
+      if (!err) {
+        console.log(conversationId, question, userResult.rows[0].user_id);
+        client.query(
+          `INSERT INTO messages (conversation_id, message_text, user_id ) VALUES ($1, $2, $3) RETURNING *`,
+          [conversationId, question, userResult.rows[0].user_id],
+          (err, conversationResult1) => {
+            if (!err) {
+              answerMessage.push(conversationResult1.rows[0]);
+              client.query(
+                `INSERT INTO messages (conversation_id, message_text, user_id ) VALUES ($1, $2, $3) RETURNING *`,
+                [conversationId, answerText, 3],
+                (err, conversationResult2) => {
+                  if (!err) {
+                    answerMessage.push(conversationResult2.rows[0]);
+                    console.log(answerMessage);
+                    res.json(answerMessage);
+                  } else {
+                    res.status(500).json({ error: err.message });
+                  }
+                }
+              );
+            } else {
+              res.status(500).json({ error: err.message });
+            }
+          }
+        );
       }
     }
   );
